6e41f3a2a05c3bae122229166f19c4b76ea87ced
“Sabrina <angellerinac@gmail.com>
2022-07-18 01:52:22 -0400

    updated products

--- frontend/pages/api/stripe.js
+++ frontend/pages/api/stripe.js
@@ -1,47 +1,4 @@
-// // Similar to express
-
-// import Stripe from "stripe";
-
-// const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`);
-
-// export default async function handler(req, res) {
-//   if (req.method === "POST") {
-//     try {
-//       //   Create Checkout Session
-//       const session = await stripe.checkout.session.create({
-//         submit_type: "pay",
-//         mode: "payment",
-//         payment_method_types: ["card"],
-//         shipping_address_collection: {
-//           allowed_countries: ["US", "CA"],
-//         },
-//         line_items: req.body.map((item) => {
-//           return {
-//             price_data: {
-//               currency: "usd",
-//               product_data: {
-//                 name: item.title,
-//                 images: [item.image.data.attributes.formats.thumbnail.url],
-//               },
-//               unit_amount: item.price * 100,
-//             },
-//             adjustable_quantity: {
-//               enabled: true,
-//               minimum: 1,
-//             },
-//             quantity: item.quantity,
-//           };
-//         }),
-//         // Show Success or Failed page
-//         success_url: `${req.headers.origin}/success`,
-//         cancel_url: `${req.headers.origin}/canceled`,
-//       });
-//       res.status(200).json(session);
-//     } catch (error) {
-//       res.status(error.statusCode || 500).json(error.nessage);
-//     }
-//   }
-// }
+//Similar to express
 
 import Stripe from "stripe";
 const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`);
@@ -51,8 +8,6 @@ export default async function handler(req, res) {
   const session = getSession(req, res);
 
   const user = session?.user;
-
-  console.log(user);
 
   if (user) {
     const stripeId = user["http://localhost:3000/stripe_customer_id"];
